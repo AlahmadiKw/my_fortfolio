9ed853297279fcc2d0edd67a90b1ff62bb16f1c2
mhd1991 <m91.alahmadi@gmail.com>
2014-09-22 23:21:03 -0700

    added persistent headers YAAAAAAAY

--- js/app.js
+++ js/app.js
@@ -1,6 +1,60 @@
 
+// this function is for persistent header
+// source: http://css-tricks.com/persistent-headers/
+function UpdateTableHeaders() {
+   $(".persist-area").each(function() {
+
+       var el             = $(this),
+           offset         = el.offset(),
+           scrollTop      = $(window).scrollTop(),
+           floatingHeader = $(".floatingHeader", this)
+
+       if ((scrollTop > offset.top) && (scrollTop < offset.top + el.height())) {
+           floatingHeader.css({
+            "visibility": "visible"
+           });
+       } else {
+           floatingHeader.css({
+            "visibility": "hidden"
+           });
+       };
+   });
+}
+
 
 $(document).ready(function() {
+
+	// for persistent header
+	var clonedHeaderRow;
+
+	$(".persist-area").each(function() {
+		clonedHeaderRow = $(".persist-header", this);
+		clonedHeaderRow
+			.before(clonedHeaderRow.clone())
+			.css("width", clonedHeaderRow.width())
+			.addClass("floatingHeader");
+	});
+
+	$(window)
+		.scroll(UpdateTableHeaders)
+		.trigger("scroll");
+
+	// disable persistent for small screen sized
+	$(window).resize(function(){
+		if ($('.main-nav li').css('display') == 'block') {
+			// console.log('yaaaaaay');
+			$('.primary-container').removeClass('persist-area');
+			$('.main-nav').removeClass('persist-header');
+		} else {
+			// console.log('lala');
+			$('.primary-container').addClass('persist-area');
+			$('.main-nav').addClass('persist-header');
+		}
+	});
+
+
+	// replace tags with keyword
+	$('.tags').text('Keywords:');
 
 	$(function() {
 		$( ".accordion" ).accordion({
